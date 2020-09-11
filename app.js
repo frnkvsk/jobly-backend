7152b4757f450834963d1dcbc287c971ab41adcf
frnkvsk <frnk05vas@zoho.com>
2020-09-11 11:28:57 -0700

    updated app.js

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ const morgan = require("morgan");
 app.use(morgan("tiny"));
 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if(req.method === 'OPTION') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+    return res.status(200).json({});
+  }
   next();
 });
 
